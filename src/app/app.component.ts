8477afb90ec8d1b8406e4a3a230434b5243a491d
julien <julien.rittl@gmail.com>
2024-11-20 13:54:42 +0100

    import assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faFacebookF, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import { faMobile } from '@fortawesome/free-solid-svg-icons';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, FontAwesomeModule],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'omees-web';
-  
-  faFacebook = faFacebookF;
-  faTwitter = faTwitter;
-  faInstagram = faInstagram;
-  faLinkedin = faLinkedin;
-  faYoutube = faYoutube;
-  faMobile = faMobile;
-  faUser = faUser;
 }
