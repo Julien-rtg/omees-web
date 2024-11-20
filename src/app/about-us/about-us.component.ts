f0b7144c4782677bb4cc0dd0f41ac16a4251a9ac
ORGA Alexis <alexis.orga@ynov.com>
2024-11-20 14:38:28 +0100

    contact form work in progress

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../component/common/navbar/navbar.component';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
