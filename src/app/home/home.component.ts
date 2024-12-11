498bc84fecccc48050dbdaabff868f95b4fdab5c
julien <julien.rittl@gmail.com>
2024-12-11 10:00:57 +0100

    before merge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SliderComponent } from '../component/common/slider/slider.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
