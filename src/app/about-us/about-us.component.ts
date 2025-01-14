af28250919f50c36ec53ea534689a097a76bf4ad
ORGA Alexis <alexis.orga@ynov.com>
2025-01-14 15:11:33 +0100

    footer

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../component/common/navbar/navbar.component';
 import { SliderComponent } from '../component/slider/slider.component';
+import { FooterComponent } from "../component/common/footer/footer.component";
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [NavbarComponent, SliderComponent],
+  imports: [NavbarComponent, SliderComponent, FooterComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss'
 })
