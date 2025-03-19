3ad4031ba511c86f10ac3cad6d8e38ca5d4735eb
julien <julien.rittl@gmail.com>
2025-03-19 10:10:49 +0100

    home responsive

--- src/app/component/common/horizontal-content/horizontal-content.component.ts
+++ src/app/component/common/horizontal-content/horizontal-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, HostListener } from '@angular/core';
 import { NgStyle, NgClass } from '@angular/common';
 
 @Component({
@@ -8,10 +8,28 @@ import { NgStyle, NgClass } from '@angular/common';
   templateUrl: './horizontal-content.component.html',
   styleUrl: './horizontal-content.component.scss'
 })
-export class HorizontalContentComponent {
+export class HorizontalContentComponent implements OnInit {
   @Input() title = ""
   @Input() content = ""
   @Input() image = ""
   @Input() isImageRight = true
   @Input() isCropped = false
+  
+  isMobile = false;
+  
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+  
+  @HostListener('window:resize')
+  checkScreenSize() {
+    this.isMobile = window.innerWidth < 768;
+  }
+  
+  getFlexDirection() {
+    if (this.isMobile) {
+      return 'column';
+    }
+    return this.isImageRight ? 'row-reverse' : 'row';
+  }
 }
