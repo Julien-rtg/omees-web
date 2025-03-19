a02d632d680239aef8c35561ad605bb6b1684cc9
julien <julien.rittl@gmail.com>
2025-03-19 10:04:45 +0100

    navbar

--- src/app/component/common/navbar/navbar.component.ts
+++ src/app/component/common/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
